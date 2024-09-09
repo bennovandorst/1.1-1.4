307278a620d16befb5506585ed6617b98177c41b
bennovandorst <9024828@student.zadkine.nl>
2024-09-09 12:05:03 +0200

    [🇳🇱] 1.2

---
+++ op2.php
@@ -0,0 +1,20 @@
+<?php
+    // Goed
+    $netherlands = "Dutch";
+    $italy = "Italian";
+    $france = "French";
+    $germany = "German";
+    $sweden = "Swedish";
+    // Fout
+    $_finland = "Finnish";
+    $123denmark = "Danish";
+    $Denederlanden = "Dutch";
+    $HOI = "Yo";
+    $U.S.A = "🦅";
+    // Correctie
+    $finland = "Finnish";
+    $d123 = "Danish";
+    $deNederlanden = "Dutch";
+    $hoi = "Yo";
+    $usa = "🦅";
+    
