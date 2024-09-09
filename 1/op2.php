a8ca16fbd7f797c2649d5b6b19fb838c72f75428
bennovandorst <9024828@student.zadkine.nl>
2024-09-09 14:45:23 +0200

    [👍] 3.1

--- 1/op2.php
+++ 1/op2.php
@@ -17,4 +17,3 @@
     $deNederlanden = "Dutch";
     $hoi = "Yo";
     $usa = "🦅";
-    
---
+++ 3/op1.php
@@ -0,0 +1,7 @@
+<?php
+
+$cities = ['Amsterdam', 'Rotterdam', 'Utrecht', 'Breda', 'Eindhoven'];
+
+echo $cities[0];
+echo $cities[3];
+echo $cities[4];
