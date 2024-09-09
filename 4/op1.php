ad6d09ccfb5f4dd70254a2faddcb886ac48f0bfa
bennovandorst <9024828@student.zadkine.nl>
2024-09-09 21:42:27 +0200

    [😼] 4.1

---
+++ 4/op1.php
@@ -0,0 +1,19 @@
+<?php
+
+echo "<pre>";
+
+$products = [
+    [ "name" => "Laptop", "price" => 999.99, "stock" => 30 ],
+    [ "name" => "Smartphone", "price" => 699.99, "stock" => 30 ],
+    [ "name" => "Monitor", "price" => 199.99, "stock" => 30 ],
+    [ "name" => "Toetsenbord", "price" => 49.99, "stock" => 30 ]
+];
+
+echo $products[1]["name"]." kost ".$products[1]["price"]." euro";
+echo "<br>";
+
+$products[3] = [ "name" => "Toetsenbord", "price" => 49.99, "stock" => 120 ];
+
+$products[] = [ "name" => "Tablet", "price" => 299.99, "stock" => 50 ];
+echo print_r($products);
+
