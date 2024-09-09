7fd9b97d471b24375f48681afef0c0e6372b2b51
bennovandorst <9024828@student.zadkine.nl>
2024-09-09 11:50:02 +0200

    [🧃] 1.1

---
+++ op1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="UTF-8">
+<title></title>
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<link rel="stylesheet" href="">
+<body>
+<?php
+$firstName = 'Jan';
+$age = '25';
+$height = '1.75';
+$isStudent = 'true';
+
+echo $firstName;
+echo $age;
+echo $height;
+echo $isStudent;
+?>
+</body>
+</html>
+
