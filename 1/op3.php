914eea35badea2e79e6fa4654445c4fc68ccf7b2
bennovandorst <9024828@student.zadkine.nl>
2024-09-10 12:51:56 +0200

    [💯] 1.3

---
+++ 1/op3.php
@@ -0,0 +1,11 @@
+<?php
+
+$city = "Amsterdam";
+$population = 821752;
+$averageTemperature = 17.5;
+$isCapital = true;
+
+echo $city."<br>";
+echo $population."<br>";
+echo $averageTemperature."<br>";
+echo $isCapital = false;
